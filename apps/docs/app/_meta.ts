d12d6af64a84bf73d2640ee5714181311604511d
mikecoj <mihai040493@gmail.com>
2026-03-16 11:17:55 +0200

    feat(docs): ✨ add comprehensive documentation for advanced-config

---
+++ apps/docs/app/_meta.ts
@@ -0,0 +1,17 @@
+export default {
+  index: {
+    title: 'Home',
+    type: 'page',
+    theme: {
+      layout: 'full',
+      sidebar: false,
+      toc: false,
+      navbar: true,
+      footer: true,
+    },
+  },
+  docs: {
+    title: 'Packages',
+    type: 'page',
+  },
+};
