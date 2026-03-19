21c88817e37a1e1a9ef93cfeabc864916e1c9bcd
mikecoj <mihai040493@gmail.com>
2026-03-19 11:42:31 +0200

    style(config): 💄 reorder export statements for consistency

--- packages/advanced-config/src/index.ts
+++ packages/advanced-config/src/index.ts
@@ -1,23 +1,24 @@
 export { AdvancedConfigModule } from './lib/advanced-config.module';
 export { ConfigService } from './lib/config.service';
 export { ConfigStore } from './lib/config-store';
-export { defineConfig } from './lib/define-config';
-export { EnvSource } from './lib/loaders';
 export { ADVANCED_CONFIG_OPTIONS, CONFIG_STORE } from './lib/constants';
-
+export { defineConfig } from './lib/define-config';
 export type {
-  IEnvSource,
-  LoadContext,
-  SecretSource,
-  FileSource,
-  ConfigLoader,
-  ConfigDefinitionOptions,
+  AdvancedConfigModuleAsyncOptions,
+  AdvancedConfigModuleOptions,
+  AdvancedConfigOptionsFactory,
+  CombineConfigs,
   ConfigDefinition,
   ConfigDefinitionInput,
+  ConfigDefinitionOptions,
   ConfigExplanation,
-  AdvancedConfigModuleOptions,
-  AdvancedConfigModuleAsyncOptions,
-  AdvancedConfigOptionsFactory,
+  ConfigLoader,
+  FileSource,
+  IEnvSource,
+  InferConfigMap,
+  LoadContext,
+  SecretSource,
 } from './lib/interfaces';
+export { EnvSource } from './lib/loaders';
 
-export type { Path, PathValue } from './lib/types';
+export type { DeepReadonly, Path, PathValue, UnionToIntersection } from './lib/types';
