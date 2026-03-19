2a9c3296ba3516e3314a983aa2fbf629791c9e9f
mikecoj <mihai040493@gmail.com>
2026-03-19 11:43:05 +0200

    style(tests): 💄 standardize variable access syntax

--- apps/demo-e2e/src/support/global-teardown.ts
+++ apps/demo-e2e/src/support/global-teardown.ts
@@ -1,10 +1,9 @@
 import { killPort } from '@nx/node/utils';
-/* eslint-disable */
 
-module.exports = async function () {
+module.exports = async () => {
   // Put clean up logic here (e.g. stopping services, docker-compose, etc.).
   // Hint: `globalThis` is shared between setup and teardown.
-  const port = process.env['PORT'] ? Number(process.env['PORT']) : 3000;
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await killPort(port);
-  console.log((globalThis as Record<string, unknown>)['__TEARDOWN_MESSAGE__']);
+  console.log((globalThis as Record<string, unknown>).__TEARDOWN_MESSAGE__);
 };
